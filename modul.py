4e8b4fbb3b543e8b83fe8e47fbd898c2d74f8af2
Artem Bardyshev <bardysevartem304@gmail.com>
2025-02-06 00:09:00 +0300

    Get binary packages function
    Initial commit. There was created function to get
    binary packages from certain branch.

---
+++ modul.py
@@ -0,0 +1,32 @@
+import requests
+
+
+# API base url
+api_url = "https://rdb.altlinux.org/api/"
+
+# API method url
+export_method = "export/branch_binary_packages/"
+
+
+def get_packages(branch: str):
+    """
+    Get binary packages from branch.
+
+
+    Args:
+        branch (str): branch name
+
+    Returns:
+        str: json response object
+    """
+
+    # Getting response from API
+    response = requests.get(api_url + export_method + branch)
+
+    # Verifying status code
+    if response.status_code != 200:
+        raise Exception(
+            f"get_packages() get {response.status_code} status code for branch '{branch}'."
+            )
+    
+    return response.json()
