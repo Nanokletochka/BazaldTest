4cfcb7b32372eccc5e9c7f77c8a064efcd74fdac
Artem Bardyshev <bardysevartem304@gmail.com>
2025-02-09 14:49:54 +0300

    Function in cli.py renamed
    existing() --> unique().

--- cli.py
+++ cli.py
@@ -6,7 +6,6 @@ from module import *
 @click.group()
 def cli():
     """CLI utility for comparing binary packages."""
-
     pass
 
 
@@ -16,15 +15,14 @@ def cli():
 @click.option(
     '--limit', type=int, default=None, help='Limit the number of packages for comparison.'
 )
-def existing(branch1, branch2, limit):
+def unique(branch1, branch2, limit):
     """Compare existing packages between two branches."""
 
     pkg_1_json = get_packages(branch1)
     pkg_2_json = get_packages(branch2)
     result = compare_existing(pkg_1_json, pkg_2_json, limit)
-    click.echo("=============== RESULTS ==============")
+    click.echo("\n")
     click.echo(dumps(result, indent=4))
-    click.echo("=============== END ==============")
 
 
 @cli.command()
@@ -39,9 +37,8 @@ def rpm(branch1, branch2, limit):
     pkg_1_json = get_packages(branch1)
     pkg_2_json = get_packages(branch2)
     result = compare_rpm(pkg_1_json, pkg_2_json, limit)
-    click.echo("=============== RESULTS ==============")
+    click.echo("\n")
     click.echo(dumps(result, indent=4))
-    click.echo("=============== END ==============")
 
 
 if __name__ == '__main__':
